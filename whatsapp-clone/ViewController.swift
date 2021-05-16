37abfb1cf8067aaa1e96bb11d4cda9926b2f9181
Neezar <rforlyffe@gmaicl.com>
2021-05-16 17:15:40 +0800

    init

---
+++ whatsapp-clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  whatsapp-clone
+//
+//  Created by Rose Neezar on 16/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
