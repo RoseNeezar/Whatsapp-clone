8e85f52c0ad8520583c65991e85b87084ce2876b
Neezar <rforlyffe@gmaicl.com>
2021-05-31 18:56:59 +0800

    Map IBoutlets

--- whatsapp-clone/LoginViewController.swift
+++ whatsapp-clone/LoginViewController.swift
@@ -8,13 +8,48 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    //MARK: - IBOutlets
+    
+    @IBOutlet weak var emailLabelOutlet: UILabel!
+    @IBOutlet weak var passwordLabelOutlet: UILabel!
+    @IBOutlet weak var repeatPasswordLabel: UILabel!
+    
+    @IBOutlet weak var signUpLabel: UILabel!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var repeatPasswordTextField: UITextField!
+    
+    @IBOutlet weak var loginButtonOutlet: UIButton!
+    @IBOutlet weak var signUpButtonOutlet: UIButton!
+    @IBOutlet weak var resendEmailButtonOutlet: UIButton!
+    
+    @IBOutlet weak var repeatPasswordLineView: UIView!
+    
+    //MARK: - Viee Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
       
     }
 
+    //MARK: - IBActions
+    @IBAction func forgotPasswordButtonPressed(_ sender: Any) {
+        
+    }
+    @IBAction func loginButtonPressed(_ sender: Any) {
+        
+    }
+    @IBAction func resendEmailButtonPressed(_ sender: Any) {
+        
+    }
+    @IBAction func signUpButtonPressed(_ sender: Any) {
+        
+    }
+    
+    
 
 }
 
