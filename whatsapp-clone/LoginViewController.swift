6435bf9d6487cb134b7f83245dc6d577b4402798
Neezar <rforlyffe@gmaicl.com>
2021-06-02 11:13:01 +0800

    Animate auth

--- whatsapp-clone/LoginViewController.swift
+++ whatsapp-clone/LoginViewController.swift
@@ -10,28 +10,30 @@ import UIKit
 class LoginViewController: UIViewController {
     
     //MARK: - IBOutlets
-    
     @IBOutlet weak var emailLabelOutlet: UILabel!
     @IBOutlet weak var passwordLabelOutlet: UILabel!
     @IBOutlet weak var repeatPasswordLabel: UILabel!
     
     @IBOutlet weak var signUpLabel: UILabel!
-    
+    //MARK: - textfields
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var repeatPasswordTextField: UITextField!
-    
+    //MARK: - buttons
     @IBOutlet weak var loginButtonOutlet: UIButton!
     @IBOutlet weak var signUpButtonOutlet: UIButton!
     @IBOutlet weak var resendEmailButtonOutlet: UIButton!
-    
+    //MARK: - views
     @IBOutlet weak var repeatPasswordLineView: UIView!
     
-    //MARK: - Viee Lifecycle
+    //MARK: - Vars
+    var isLogin = true
     
+    //MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUIFor(login: true)
       setupTextFieldDelegate()
     }
 
@@ -45,8 +47,9 @@ class LoginViewController: UIViewController {
     @IBAction func resendEmailButtonPressed(_ sender: Any) {
         
     }
-    @IBAction func signUpButtonPressed(_ sender: Any) {
-        
+    @IBAction func signUpButtonPressed(_ sender: UIButton) {
+        updateUIFor(login: sender.titleLabel?.text == "Login")
+        isLogin.toggle()
     }
     
     //MARK: - Setup
@@ -61,6 +64,18 @@ class LoginViewController: UIViewController {
     }
     
     //MARK: - Animations
+    private func updateUIFor(login: Bool){
+        loginButtonOutlet.setImage(UIImage(named: login ? "loginBtn" : "registerBtn"), for: .normal)
+        signUpButtonOutlet.setTitle(login ? "SignUp" : "Login", for: .normal)
+        signUpLabel.text = login ? "Don't have an account?" : "Have an account?"
+        
+        UIView.animate(withDuration: 0.4) {
+            self.repeatPasswordLabel.isHidden = login
+            self.repeatPasswordTextField.isHidden = login
+            self.repeatPasswordLineView.isHidden = login
+        }
+    }
+    
     private func updatePlaceholderLabels(textField: UITextField){
         switch textField {
         case emailTextField:
